dc9354e53ade7954494602f6527f6ae077a98d97
seigle15 <sean.eigle143@myci.csuci.edu>
2022-05-13 16:56:44 -0700

    Fixed aim script

--- Assets/Scripts/PlayerAimWeapon.cs
+++ Assets/Scripts/PlayerAimWeapon.cs
@@ -15,16 +15,17 @@ public class PlayerAimWeapon : MonoBehaviour
 
     private void Update()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0f;
-        //if (Input.GetButtonDown("Fire1"))
-       // {
-            Vector3 aimDirection = (mousePos - transform.position).normalized;
-            float angle = Mathf.Atan2(aimDirection.x, aimDirection.y) * Mathf.Rad2Deg;
-            aimTransform.eulerAngles = new Vector3(0, 0, angle);
-
-        //}
+        //Keeps the 
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        
+        if (Input.GetMouseButton(0))
+        {
+            aimTransform.rotation = Quaternion.Euler(0f, 0f, angle);
+            Debug.Log("Holding down mouse");
+        }
     }
-    
     
 }
